fff83f2dfa32e690582e5d187c33e1b488a29193
NoScript <weerawat.wic@msu.ac.th>
2019-04-04 12:05:33 +0700

    TestGit

--- Player.cs
+++ Player.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Skill;
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("123");
         Debug.Log("ATK: "+Ranger.SkillPassiveFocus.atk);
         Debug.Log("SPD: "+Ranger.SkillPassiveFocus.spd);
         Debug.Log("Name: "+Ranger.SkillPassiveFocus.names);
